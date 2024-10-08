5fc3131687aefc0849f7075a369c32840d2df40d
Ajay-muthusamy <ajaym.22cse@kongu.edu>
2024-10-08 15:30:32 +0530

    json

--- dbConnect/database.js
+++ dbConnect/database.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import 'dotenv/config'
 
-const uri = process.env.mongodb_uri;
+const uri = "mongodb+srv://ajaymdu18:aSTpDhF0DR4sgL4h@cluster0.07kut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 export const dbconnect = async()=>{
     try {
